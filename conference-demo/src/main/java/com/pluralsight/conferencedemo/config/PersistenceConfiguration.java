d6da8c576cf05455830faee328f7b214881e7057
David Mordechai <david.mordechai79@gmail.com>
2021-12-26 18:49:39 +0200

    added docker support, dockerfiles and docker-compose

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -6,15 +6,15 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
-public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("Welcome");
-        System.out.println("My custom datasource bean has been initialized and set");
-        return builder.build();
-    }
-}
+//@Configuration
+//public class PersistenceConfiguration {
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("Welcome");
+//        System.out.println("My custom datasource bean has been initialized and set");
+//        return builder.build();
+//    }
+//}
